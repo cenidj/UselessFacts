db3f0bb847d7b0af2e7e51dc9fd55ac4d1f57c92
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-10-21 14:26:35 -0400

    Initial Commit

---
+++ UselessFacts/UselessFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UselessFactsApp.swift
+//  UselessFacts
+//
+//  Created by Cesario Nivar on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct UselessFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
